bd019c795676ddb10c0a0a4b85a5eeb53dfc3c82
Nitesh Kumar Singh <nitesh.ldd@gmail.com>
2023-01-16 00:43:31 +0530

    Updated temp and freq

--- server/server.js
+++ server/server.js
@@ -28,10 +28,10 @@ app.post('/', async(req, res)=>{
         const response = await openai.createCompletion({
             model:"text-davinci-003",
             prompt:`${prompt}`,
-            temperature:0.2,
+            temperature:0,
             max_tokens:3000,
             top_p:1,
-            frequency_penalty:0.5,
+            frequency_penalty:0,
             presence_penalty:0,
         });
         res.status(200).send({
